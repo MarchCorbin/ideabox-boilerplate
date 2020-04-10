abd7893a22b67f5045c3c6990d02505a50b31724
Suzie Q <suzieqwolf@gmail.com>
2020-04-10 02:46:52 -0600

    Query selectors and event listener adds functionality to menu button in responsive view

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var hamButton = document.querySelector(".ham-button");
+var mainPage = document.querySelector(".ideas");
+var menu = document.querySelector('.menu');
+var leftSideText = document.querySelector('.left-side-text');
+var leftSideBorder = document.querySelector('.left-side-border');
+var hamHide = document.querySelector('.ham-hide');
+
+
+hamButton.addEventListener("click", expandLeftSide)
