47acd49f22e7687477413e5596be24892bc94935
Suzie Q <suzieqwolf@gmail.com>
2020-04-10 02:56:54 -0600

    JS functions for menu view in responsive layout, began testing, not fully functional

--- src/main.js
+++ src/main.js
@@ -6,4 +6,35 @@ var leftSideBorder = document.querySelector('.left-side-border');
 var hamHide = document.querySelector('.ham-hide');
 
 
-hamButton.addEventListener("click", expandLeftSide)
+hamButton.addEventListener("click", expandLeftSide) 
+  
+
+function expandLeftSide(event) {
+
+    if (event.target.src !== "Assets/menu-close.svg") {
+         
+        mainPage.classList.add("purple-3");
+        leftSideBorder.classList.add("left-side-border-expand");
+        leftSideText.classList.add("left-side-text-expand");
+        menu.classList.add("menu-expand");
+        menu.classList.remove("menu");
+        hamHide.src = "Assets/menu-close.svg";
+    }
+    // } else if (event.target.src !== "Assets/menu.svg") {
+
+    //     console.log("hello");
+
+    //     mainPage.classList.remove("purple-3");
+    //     leftSideBorder.classList.remove("left-side-border-expand");
+    //     leftSideText.classList.remove("left-side-text-expand");
+    //     menu.classList.remove("menu-expand");
+    //     menu.classList.add("menu");
+    //     hamHide.src = "Assets/menu.svg";  
+    // }
+}
+    
+
+
+// function() {
+
+// }    
