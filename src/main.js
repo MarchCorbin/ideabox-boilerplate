f339f57ebdb6d96a5d8f9ea4a1140092780e415a
Suzie Q <suzieqwolf@gmail.com>
2020-04-11 16:07:34 -0600

    Style changes for grid layout of displayed ideaCard(s) in desktop view

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ var ideaCards = document.querySelector('.idea-cards')
 
 hamButton.addEventListener("click", toggleButton)
 saveButton.addEventListener('click', function(){
-  saveToStorage()
+  saveToIdeasArr()
   displayCards()})
 titleInput.addEventListener('input', checkInputs)
 bodyInput.addEventListener('input', checkInputs)
@@ -58,6 +58,39 @@ function retractLeftSide(){
   saveButton.classList.remove('disabled-button')
   saveButton.classList.add('save-button')
   saveButton.disabled = false
-
 }
+}
+
+function saveToIdeasArr() {
+  if(titleInput.value !== '' || bodyInput.value !== ''){
+  var title = titleInput.value
+  var body = bodyInput.value
+  var fullCard = new Idea(title, body)
+  savedIdeas.push(fullCard)
+  titleInput.value = bodyInput.value = ''
+  } else {
+  checkInputs()
+  }
+}
+
+function displayCards() {
+  for (var i = 0; i < savedIdeas.length; i++) {
+var newCard = savedIdeas[i]
+console.log(newCard.id)
+var theStuff = `<section class="mini-card" id=${newCard.id}>
+    <section class="idea-card-top">
+      <button type="button" name="star-active" class="star-active-button"><img src="Assets/star-active.svg" class="star-active-img"/></button>
+      <button type="button" name="delete" class="delete-button"><img src="Assets/delete.svg" class="delete-idea-img"/></button>
+    </section>
+    
+      <h4 class="idea-card-title">${newCard.title}</h4>
+      <text class="idea-card-body">${newCard.body}</text>
+    
+    <section class="idea-card-bottom">
+       <button type="button" name="comment" class="comment-button"><img src="Assets/comment.svg" class="comment-idea-img"/></button>
+       <p class="comment-text">Comment</p>
+    </section>
+  </section>`
+ideaCards.insertAdjacentHTML('afterbegin', theStuff)
+  }
 }
