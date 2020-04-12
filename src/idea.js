d41a0eb70c446b8e18d2cfe62bafafdb66c3a09c
Corbin March <marchcorbin@gmail.com>
2020-04-12 14:27:40 -0600

    delete functionality working

--- src/idea.js
+++ src/idea.js
@@ -2,20 +2,20 @@ class Idea {
   constructor(title, body) {
     this.title = title
     this.body = body
-    //this.star = false
+    this.star = false
     this.id = Date.now()
-    console.log(this, "Hello")
   }
 
 
   saveToStorage() {
-    console.log(this, "Help")
     var ideaToStore = {title: this.title, body: this.body, id: this.id}
     var stringifiedIdea = JSON.stringify(ideaToStore)
-    localStorage.setItem("storedIdea", stringifiedIdea)
+    localStorage.setItem(`storedIdea${this.id}`, stringifiedIdea)
+  }
+
+  deleteFromStorage(){
+    localStorage.removeItem(`storedIdea${this.id}`)  
   }
 
 
 }
-
-
