41a3edcba0f994408d3b377957971e2c13dbc369
Suzie Q <suzieqwolf@gmail.com>
2020-04-11 20:36:36 -0600

    Adds localStorage function but NOT fully functional.

--- src/idea.js
+++ src/idea.js
@@ -1,10 +1,21 @@
 class Idea {
-  constructor(title, body){
+  constructor(title, body) {
     this.title = title
     this.body = body
-    this.star = false
+    //this.star = false
     this.id = Date.now()
+    console.log(this, "Hello")
   }
+
+
+  saveToStorage() {
+    console.log(this, "Help")
+    var ideaToStore = {title: this.title, body: this.body, id: this.id}
+    var stringifiedIdea = JSON.stringify(ideaToStore)
+    localStorage.setItem("storedIdea", stringifiedIdea)
+  }
+
+
 }
 
 
